4bd78170427dde51a1a2c105e0b42d17e5fa644d
Christopher Svec <csvec@irobot.com>
2022-10-06 20:45:04 -0400

    Added color buffer, makes a big ol' yellow window.

--- src/main.c
+++ src/main.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
-SDL_Window *window = NULL;
-SDL_Renderer *renderer = NULL;
+SDL_Window * window = NULL;
+SDL_Renderer * renderer = NULL;
+
+SDL_Texture * color_buffer_texture = NULL;
+uint32_t * color_buffer = NULL;
+
 bool is_running = false;
+const int window_width = 800;
+const int window_height = 600;
+
 
 bool initialize_window(void)
 {
@@ -18,8 +26,8 @@ bool initialize_window(void)
 		NULL,
 		SDL_WINDOWPOS_CENTERED,
 		SDL_WINDOWPOS_CENTERED,
-		800,
-		600,
+		window_width,
+		window_height,
 		SDL_WINDOW_BORDERLESS
 	);
 	// TODO 
@@ -31,16 +39,37 @@ bool initialize_window(void)
 	// Create SDL render
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (! renderer) {
-		fprintf(stderr, "Error: SDL_CreateRendere() failed\n");
+		fprintf(stderr, "Error: SDL_CreateRender failed\n");
 		return false;
 	}
 
     return true;
 }
 
-void setup(void)
+bool setup(void)
 {
-	// TODO:
+	color_buffer = (uint32_t *)malloc(window_width * window_height * sizeof(uint32_t));
+	
+	if (!color_buffer) {
+		fprintf(stderr, "Error: malloc failed for color_buffer.\n");
+		return false;
+	}
+
+    // Create a texture buffer that displays the color buffer.
+	color_buffer_texture = SDL_CreateTexture(
+		renderer,
+		SDL_PIXELFORMAT_ARGB8888,
+		SDL_TEXTUREACCESS_STREAMING,
+		window_width,
+		window_height
+	);
+
+    if (! color_buffer_texture) {
+		fprintf(stderr, "Error: SDL_CreateTexture failed\n");
+		return false;
+	}
+
+	return true;
 }
 
 void process_input(void)
@@ -68,19 +97,71 @@ void update(void)
 	// TODO:
 }
 
+void set_color_buffer_pixel_color(int x, int y, uint32_t color)
+{
+	color_buffer[(window_width * y) + x] = color;
+}
+
+void clear_color_buffer(uint32_t color)
+{
+    for (uint32_t y = 0; y < window_height; y++) {
+		for (uint32_t x = 0; x < window_width; x++) {
+			set_color_buffer_pixel_color(x, y, color);
+		}
+	}
+}
+
+void render_color_buffer(void) {
+    SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		window_width * sizeof(uint32_t)
+	);
+	SDL_RenderCopy(
+		renderer,
+		color_buffer_texture,
+		NULL,
+		NULL
+	);
+}
+
 void render(void)
 {
 	SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
 	SDL_RenderClear(renderer);
 
+    render_color_buffer();
+
+	clear_color_buffer(0xFFFFFF00);
+
 	SDL_RenderPresent(renderer);
+}
+
+void destroy_window(void)
+{
+	if (color_buffer) {
+		free(color_buffer);
+		color_buffer = NULL;
+	}
+	if (renderer) {
+		SDL_DestroyRenderer(renderer);
+	}
+	if (window) {
+		SDL_DestroyWindow(window);
+    }
+
+	SDL_Quit();
 }
 
 int main(void) {
 
 	is_running = initialize_window();
 
-	setup();
+	if (! setup()) {
+		// If setup failed, don't run.
+		is_running = false;
+	}
 
 	while (is_running) {
 		process_input();
@@ -88,5 +169,6 @@ int main(void) {
 		render();
 	}
 
+    destroy_window();
 	return 0;
 }
