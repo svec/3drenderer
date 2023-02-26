579e123f359846c757e9bf07732b692c0ace51e0
Christopher Svec <christophersvec@gmail.com>
2023-02-26 17:31:44 -0500

    Added more vector operations.

--- src/gfx-vector.c
+++ src/gfx-vector.c
@@ -6,9 +6,85 @@ float vec2_length(vec2_t v)
     return sqrt(v.x * v.x + v.y * v.y);
 }
 
+vec2_t vec2_add(vec2_t a, vec2_t b)
+{
+    vec2_t result = {
+        .x = a.x + b.x,
+        .y = a.y + b.y
+    };
+    return result;
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b)
+{
+    vec2_t result = {
+        .x = a.x - b.x,
+        .y = a.y - b.y
+    };
+    return result;
+}
+
+vec2_t vec2_mul(vec2_t v, float factor)
+{
+    vec2_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor
+    };
+    return result;
+}
+
+vec2_t vec2_div(vec2_t v, float factor)
+{
+    vec2_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor
+    };
+    return result;
+}
+
 float vec3_length(vec3_t v)
 {
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b)
+{
+    vec3_t result = {
+        .x = a.x + b.x,
+        .y = a.y + b.y,
+        .z = a.z + b.z
+    };
+    return result;
+}
+
+vec3_t vec3_sub(vec3_t a, vec3_t b)
+{
+    vec3_t result = {
+        .x = a.x - b.x,
+        .y = a.y - b.y,
+        .z = a.z - b.z
+    };
+    return result;
+}
+
+vec3_t vec3_mul(vec3_t v, float factor)
+{
+    vec3_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor
+    };
+    return result;
+}
+
+vec3_t vec3_div(vec3_t v, float factor)
+{
+    vec3_t result = {
+        .x = v.x / factor,
+        .y = v.y / factor,
+        .z = v.z / factor
+    };
+    return result;
 }
 
 vec3_t vec3_rotate_x(vec3_t v, float angle)
