061f9d13a12706263a5c14d0491f9c91b3f5602a
Christopher Svec <christophersvec@gmail.com>
2023-02-28 19:44:45 -0500

    Changed tabs to spaces.

--- src/display.c
+++ src/display.c
@@ -13,144 +13,144 @@ bool use_fullscreen = false;
 
 bool initialize_window(void)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error: SDL_Init() failed\n");
-		return false;
-	}
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        fprintf(stderr, "Error: SDL_Init() failed\n");
+        return false;
+    }
 
-	// Query SDL for max fullscreen resolution.
-	SDL_DisplayMode display_mode;
-	SDL_GetCurrentDisplayMode(0, &display_mode);
+    // Query SDL for max fullscreen resolution.
+    SDL_DisplayMode display_mode;
+    SDL_GetCurrentDisplayMode(0, &display_mode);
 
-	window_width = display_mode.w;
-	window_height = display_mode.h;
+    window_width = display_mode.w;
+    window_height = display_mode.h;
 
-	if (! use_fullscreen) {
-		window_width = 800;
-		window_height = 600;
-	}
+    if (! use_fullscreen) {
+        window_width = 800;
+        window_height = 600;
+    }
 
-	// Create SDL window at the center of the screen.
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		window_width,
-		window_height,
-		SDL_WINDOW_BORDERLESS
-	);
-	// TODO 
-	if (! window) {
-		fprintf(stderr, "Error: SDL_CreateWindow() failed\n");
-		return false;
-	}
+    // Create SDL window at the center of the screen.
+    window = SDL_CreateWindow(
+        NULL,
+        SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED,
+        window_width,
+        window_height,
+        SDL_WINDOW_BORDERLESS
+    );
+    // TODO 
+    if (! window) {
+        fprintf(stderr, "Error: SDL_CreateWindow() failed\n");
+        return false;
+    }
 
-	// Create SDL render
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (! renderer) {
-		fprintf(stderr, "Error: SDL_CreateRender failed\n");
-		return false;
-	}
+    // Create SDL render
+    renderer = SDL_CreateRenderer(window, -1, 0);
+    if (! renderer) {
+        fprintf(stderr, "Error: SDL_CreateRender failed\n");
+        return false;
+    }
 
     if (use_fullscreen) {
-	    SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
-	}
+        SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+    }
 
     return true;
 }
 
 void draw_pixel(int x, int y, uint32_t color)
 {
-	if (    (x >= 0) && (x < window_width)
-	     && (y >= 0) && (y < window_height)) {
-		color_buffer[(window_width * y) + x] = color;
-	}
+    if (    (x >= 0) && (x < window_width)
+         && (y >= 0) && (y < window_height)) {
+        color_buffer[(window_width * y) + x] = color;
+    }
 }
 
 void clear_color_buffer(uint32_t color)
 {
     for (int y = 0; y < window_height; y++) {
-		for (int x = 0; x < window_width; x++) {
-			draw_pixel(x, y, color);
-		}
-	}
+        for (int x = 0; x < window_width; x++) {
+            draw_pixel(x, y, color);
+        }
+    }
 }
 
 void render_color_buffer(void)
 {
     SDL_UpdateTexture(
-		color_buffer_texture,
-		NULL,
-		color_buffer,
-		window_width * sizeof(uint32_t)
-	);
-	SDL_RenderCopy(
-		renderer,
-		color_buffer_texture,
-		NULL,
-		NULL
-	);
+        color_buffer_texture,
+        NULL,
+        color_buffer,
+        window_width * sizeof(uint32_t)
+    );
+    SDL_RenderCopy(
+        renderer,
+        color_buffer_texture,
+        NULL,
+        NULL
+    );
 }
 
 void draw_grid(void)
 {
-	const int x_incr = 10;
-	const int y_incr = 10;
-	const uint32_t color = 0xFF555555;
+    const int x_incr = 10;
+    const int y_incr = 10;
+    const uint32_t color = 0xFF555555;
 
-	for (int x=0; x < window_width; x += x_incr) {
-		for (int y=0; y < window_height; y += y_incr) {
-			draw_pixel(x, y, color);
-		}
-	}
+    for (int x=0; x < window_width; x += x_incr) {
+        for (int y=0; y < window_height; y += y_incr) {
+            draw_pixel(x, y, color);
+        }
+    }
 }
 
 void draw_rect(int rect_x, int rect_y, int width, int height, uint32_t color)
 {
     for (int x = rect_x; x < (rect_x + width); x++) {
-		for (int y = rect_y; y < (rect_y + height); y++) {
-			draw_pixel(x, y, color);
-		}
-	}
+        for (int y = rect_y; y < (rect_y + height); y++) {
+            draw_pixel(x, y, color);
+        }
+    }
 }
 
 void destroy_window(void)
 {
-	if (renderer) {
-		SDL_DestroyRenderer(renderer);
-	}
-	if (window) {
-		SDL_DestroyWindow(window);
+    if (renderer) {
+        SDL_DestroyRenderer(renderer);
+    }
+    if (window) {
+        SDL_DestroyWindow(window);
     }
 
-	SDL_Quit();
+    SDL_Quit();
 }
 
 
 void draw_line(int x0, int y0, int x1, int y1, uint32_t color)
 {
-	// Draw a line between two points using the DDA algorithm.
-	int delta_x = x1 - x0;
-	int delta_y = y1 - y0;
+    // Draw a line between two points using the DDA algorithm.
+    int delta_x = x1 - x0;
+    int delta_y = y1 - y0;
 
-	// Figure out the longest dimension of line: x or y?
-	int longest_side_length = (abs(delta_x) > abs(delta_y)) ? abs(delta_x) : abs(delta_y);
+    // Figure out the longest dimension of line: x or y?
+    int longest_side_length = (abs(delta_x) > abs(delta_y)) ? abs(delta_x) : abs(delta_y);
 
-	float inc_x = delta_x / (float)longest_side_length;
-	float inc_y = delta_y / (float)longest_side_length;
+    float inc_x = delta_x / (float)longest_side_length;
+    float inc_y = delta_y / (float)longest_side_length;
 
     float current_x = x0;
-	float current_y = y0;
+    float current_y = y0;
     for (int ii=0; ii <= longest_side_length; ii++) {
-		draw_pixel(round(current_x), round(current_y), color);
+        draw_pixel(round(current_x), round(current_y), color);
         current_x += inc_x;
-		current_y += inc_y;
-	}
+        current_y += inc_y;
+    }
 }
 
 void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color)
 {
-	draw_line(x0, y0, x1, y1, color);
-	draw_line(x1, y1, x2, y2, color);
-	draw_line(x2, y2, x0, y0, color);
+    draw_line(x0, y0, x1, y1, color);
+    draw_line(x1, y1, x2, y2, color);
+    draw_line(x2, y2, x0, y0, color);
 }
--- src/main.c
+++ src/main.c
@@ -20,202 +20,202 @@ bool is_running = false;
 
 bool setup(void)
 {
-	color_buffer = (uint32_t *)malloc(window_width * window_height * sizeof(uint32_t));
-	
-	if (!color_buffer) {
-		fprintf(stderr, "Error: malloc failed for color_buffer.\n");
-		return false;
-	}
+    color_buffer = (uint32_t *)malloc(window_width * window_height * sizeof(uint32_t));
+    
+    if (!color_buffer) {
+        fprintf(stderr, "Error: malloc failed for color_buffer.\n");
+        return false;
+    }
 
     // Create a texture buffer that displays the color buffer.
-	color_buffer_texture = SDL_CreateTexture(
-		renderer,
-		SDL_PIXELFORMAT_ARGB8888,
-		SDL_TEXTUREACCESS_STREAMING,
-		window_width,
-		window_height
-	);
+    color_buffer_texture = SDL_CreateTexture(
+        renderer,
+        SDL_PIXELFORMAT_ARGB8888,
+        SDL_TEXTUREACCESS_STREAMING,
+        window_width,
+        window_height
+    );
 
     if (! color_buffer_texture) {
-		fprintf(stderr, "Error: SDL_CreateTexture failed\n");
-		return false;
-	}
+        fprintf(stderr, "Error: SDL_CreateTexture failed\n");
+        return false;
+    }
 
-	//load_cube_mesh_data();
-	bool success = load_obj_file_data("assets/cube.obj");
-	//bool success = load_obj_file_data("assets/f22.obj");
-	if (success == false) {
-		fprintf(stderr, "Error: loading graphics obj file failed.\n");
-		return false;
-	}
+    //load_cube_mesh_data();
+    bool success = load_obj_file_data("assets/cube.obj");
+    //bool success = load_obj_file_data("assets/f22.obj");
+    if (success == false) {
+        fprintf(stderr, "Error: loading graphics obj file failed.\n");
+        return false;
+    }
 
-	return true;
+    return true;
 }
 
 void process_input(void)
 {
     SDL_Event event;
-	SDL_PollEvent(&event);
+    SDL_PollEvent(&event);
 
-	switch (event.type) {
-		case SDL_QUIT:  // User clicks window close button
-		    is_running = false;
-			break;
-		case SDL_KEYDOWN:
-		    if (event.key.keysym.sym == SDLK_ESCAPE) {
-				is_running = false;
-			}
-			break;
-		default:
-		    break;
+    switch (event.type) {
+        case SDL_QUIT:  // User clicks window close button
+            is_running = false;
+            break;
+        case SDL_KEYDOWN:
+            if (event.key.keysym.sym == SDLK_ESCAPE) {
+                is_running = false;
+            }
+            break;
+        default:
+            break;
 
-	}
+    }
 }
 
 // Project a 3D point (our 3D space) into a 2D point (for the 2D screen).
 vec2_t project(vec3_t point3d)
 {
-	// Simple perspective divide projection: 
-	//     Divide by z, so things that are further away (larger z) are smaller.
-	// Scale by fov_factor.
-	vec2_t projected_point = {
-		.x = (fov_factor * point3d.x) / point3d.z,
-		.y = (fov_factor * point3d.y) / point3d.z
-	};
-	return projected_point;
+    // Simple perspective divide projection: 
+    //     Divide by z, so things that are further away (larger z) are smaller.
+    // Scale by fov_factor.
+    vec2_t projected_point = {
+        .x = (fov_factor * point3d.x) / point3d.z,
+        .y = (fov_factor * point3d.y) / point3d.z
+    };
+    return projected_point;
 }
 
 void update(void)
 {
-	// Do we need to delay before updating the frame?
-	int time_to_wait_ms = FRAME_TARGET_TIME_MS - (SDL_GetTicks() - previous_frame_time);
+    // Do we need to delay before updating the frame?
+    int time_to_wait_ms = FRAME_TARGET_TIME_MS - (SDL_GetTicks() - previous_frame_time);
 
     if ((time_to_wait_ms > 0) && (time_to_wait_ms <= FRAME_TARGET_TIME_MS)) {
-		// Delay until it's time for the next frame.
-		SDL_Delay(time_to_wait_ms);
-	}
+        // Delay until it's time for the next frame.
+        SDL_Delay(time_to_wait_ms);
+    }
 
     // How many ms have passed since we last were called?
     previous_frame_time = SDL_GetTicks();
 
-	// Initialize the array of triangles to render.
-	triangles_to_render = NULL;
+    // Initialize the array of triangles to render.
+    triangles_to_render = NULL;
 
     // Rotate the cube by a little bit in the y direction each frame.
-	mesh.rotation.x += 0.01;
-	mesh.rotation.y += 0.01;
-	mesh.rotation.z += 0.01;
+    mesh.rotation.x += 0.01;
+    mesh.rotation.y += 0.01;
+    mesh.rotation.z += 0.01;
 
     // Loop all triangle faces.
-	int num_faces = array_length(mesh.faces);
+    int num_faces = array_length(mesh.faces);
 
-	for (int face_i=0; face_i < num_faces; face_i++) {
-		// Handle 1 triangle face per iteration.
+    for (int face_i=0; face_i < num_faces; face_i++) {
+        // Handle 1 triangle face per iteration.
 
-		face_t mesh_face = mesh.faces[face_i];
-		vec3_t face_vertices[3];
-		face_vertices[0] = mesh.vertices[mesh_face.a - 1]; // -1 because vertices are 1-indexed
-		face_vertices[1] = mesh.vertices[mesh_face.b - 1]; // -1 because vertices are 1-indexed
-		face_vertices[2] = mesh.vertices[mesh_face.c - 1]; // -1 because vertices are 1-indexed
+        face_t mesh_face = mesh.faces[face_i];
+        vec3_t face_vertices[3];
+        face_vertices[0] = mesh.vertices[mesh_face.a - 1]; // -1 because vertices are 1-indexed
+        face_vertices[1] = mesh.vertices[mesh_face.b - 1]; // -1 because vertices are 1-indexed
+        face_vertices[2] = mesh.vertices[mesh_face.c - 1]; // -1 because vertices are 1-indexed
 
         triangle_t projected_triangle;
 
         // For all 3 vertices of this triangle, apply transformations.
-		for (int vertex_i = 0; vertex_i < 3; vertex_i++)
-		{
-			vec3_t transformed_vertex = face_vertices[vertex_i];
+        for (int vertex_i = 0; vertex_i < 3; vertex_i++)
+        {
+            vec3_t transformed_vertex = face_vertices[vertex_i];
 
-			transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
-			transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
-			transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
+            transformed_vertex = vec3_rotate_x(transformed_vertex, mesh.rotation.x);
+            transformed_vertex = vec3_rotate_y(transformed_vertex, mesh.rotation.y);
+            transformed_vertex = vec3_rotate_z(transformed_vertex, mesh.rotation.z);
 
-			// Translate the vertex away from the camera.
-			transformed_vertex.z -= camera_position.z;
+            // Translate the vertex away from the camera.
+            transformed_vertex.z -= camera_position.z;
 
             // Project the current vertex.
-			vec2_t projected_point = project(transformed_vertex);
+            vec2_t projected_point = project(transformed_vertex);
 
-			projected_point.x += (window_width / 2);  // translate to center of window
-			projected_point.y += (window_height / 2); // translate to center of window
+            projected_point.x += (window_width / 2);  // translate to center of window
+            projected_point.y += (window_height / 2); // translate to center of window
 
-			projected_triangle.points[vertex_i] = projected_point;
-		}
+            projected_triangle.points[vertex_i] = projected_point;
+        }
 
         // Saves the projected triangle to the the array of triangles to render.
-		array_push(triangles_to_render, projected_triangle);
-	}
+        array_push(triangles_to_render, projected_triangle);
+    }
 }
 
 void render(void)
 {
     draw_grid();
 
-	// Loop all projected triangles and render them.
+    // Loop all projected triangles and render them.
 
-	int num_triangles = array_length(triangles_to_render);
+    int num_triangles = array_length(triangles_to_render);
 
-	for (int ii=0; ii < num_triangles; ii++) {
-		triangle_t triangle = triangles_to_render[ii];
+    for (int ii=0; ii < num_triangles; ii++) {
+        triangle_t triangle = triangles_to_render[ii];
 
-		// Draw each point as a small 4x4 yellow rectangle so we can see it.
-		draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
-		draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
-		draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
+        // Draw each point as a small 4x4 yellow rectangle so we can see it.
+        draw_rect(triangle.points[0].x, triangle.points[0].y, 3, 3, 0xFFFFFF00);
+        draw_rect(triangle.points[1].x, triangle.points[1].y, 3, 3, 0xFFFFFF00);
+        draw_rect(triangle.points[2].x, triangle.points[2].y, 3, 3, 0xFFFFFF00);
 
         // Draw the lines for the triangle.
-		draw_triangle(
-			triangle.points[0].x, 
-			triangle.points[0].y, 
-			triangle.points[1].x, 
-			triangle.points[1].y, 
-			triangle.points[2].x, 
-			triangle.points[2].y, 
-			0xFF00FF00
-		);
-	}
+        draw_triangle(
+            triangle.points[0].x, 
+            triangle.points[0].y, 
+            triangle.points[1].x, 
+            triangle.points[1].y, 
+            triangle.points[2].x, 
+            triangle.points[2].y, 
+            0xFF00FF00
+        );
+    }
 
-	// Now that we've rendered the triagles into the frame buffer, 
-	// clear the triangle array.
-	array_free(triangles_to_render);
+    // Now that we've rendered the triagles into the frame buffer, 
+    // clear the triangle array.
+    array_free(triangles_to_render);
 
     render_color_buffer();
 
-	clear_color_buffer(0xFF000000);
+    clear_color_buffer(0xFF000000);
 
-	SDL_RenderPresent(renderer);
+    SDL_RenderPresent(renderer);
 }
 
 void free_resources(void)
 {
-	array_free(mesh.faces);
-	array_free(mesh.vertices);
+    array_free(mesh.faces);
+    array_free(mesh.vertices);
 
-	if (color_buffer) {
-		free(color_buffer);
-		color_buffer = NULL;
-	}
+    if (color_buffer) {
+        free(color_buffer);
+        color_buffer = NULL;
+    }
 }
 
 int main(void) {
 
-	is_running = initialize_window();
+    is_running = initialize_window();
 
-	if (! setup()) {
-		// If setup failed, don't run.
-		printf("ERROR: setup failed.\n");
-		is_running = false;
-	}
+    if (! setup()) {
+        // If setup failed, don't run.
+        printf("ERROR: setup failed.\n");
+        is_running = false;
+    }
 
-	//is_running = false; // svechack
+    //is_running = false; // svechack
 
-	while (is_running) {
-		process_input();
-		update();
-		render();
-	}
+    while (is_running) {
+        process_input();
+        update();
+        render();
+    }
 
     destroy_window();
-	free_resources();
+    free_resources();
 
-	return 0;
+    return 0;
 }
