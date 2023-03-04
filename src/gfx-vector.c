dbf1fdb574b37a2b9e4cb0cf116d7e0e709bcd52
Christopher Svec <christophersvec@gmail.com>
2023-03-04 13:06:43 -0500

    Added dot and cross products.

--- src/gfx-vector.c
+++ src/gfx-vector.c
@@ -42,6 +42,11 @@ vec2_t vec2_div(vec2_t v, float factor)
     return result;
 }
 
+float vec2_dot(vec2_t a, vec2_t b)
+{
+    return (a.x * b.x) + (a.y * b.y);
+}
+
 float vec3_length(vec3_t v)
 {
     return sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
@@ -85,6 +90,21 @@ vec3_t vec3_div(vec3_t v, float factor)
         .z = v.z / factor
     };
     return result;
+}
+
+vec3_t vec3_cross(vec3_t a, vec3_t b)
+{
+    vec3_t result = {
+        .x = a.y * b.z - a.z * b.y,
+        .y = a.z * b.x - a.x * b.z,
+        .z = a.x * b.y - a.y * b.x
+    };
+    return result;
+}
+
+float vec3_dot(vec3_t a, vec3_t b)
+{
+    return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
 }
 
 vec3_t vec3_rotate_x(vec3_t v, float angle)
