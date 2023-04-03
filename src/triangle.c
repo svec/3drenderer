a5fd11c080a8ef71cebce969a9d6e3c265c24369
Christopher Svec <christophersvec@gmail.com>
2023-04-02 22:55:26 -0400

    Fixed a possible div-by-0.

--- src/triangle.c
+++ src/triangle.c
@@ -129,13 +129,26 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
         int_swap(&y0, &y1);
     }
 
-    // Calculate midpoint (Mx,My) using triangle similarity.
-    int My = y1;
-    int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
+    // There are 2 special cases where y0==y1 and y1==y2. In these cases, one of the
+    // fill_flat_*_triangle() functions would do a div by zero, and it turns out
+    // we don't need to call both fill_flat_*_triangle() functions in these
+    // cases anyway.
+    if (y0 == y1) {
+        // This triangle has a flat top.
+        fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
 
-    // Draw flat-bottom triangle.
-    fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+    } else if (y1 == y2) {
+        // This triangle has a flat bottom.
+        fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+    } else {
+        // Calculate midpoint (Mx,My) using triangle similarity.
+        int My = y1;
+        int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
 
-    // Draw flat-top triangle.
-    fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+        // Draw flat-bottom triangle.
+        fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
+        // Draw flat-top triangle.
+        fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+    }
 }
